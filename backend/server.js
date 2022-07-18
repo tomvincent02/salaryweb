125599cc31fdcb6607eb615013e4948df3e9f775
Tom <tomvincent02@icloud.com>
2022-07-18 13:46:49 -0500

    Not working but used terminal to search the db using mongodb shell.

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5004;
 
 app.use(cors());
 app.use(express.json());
@@ -22,7 +22,13 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Express Server is running on port: ${port}`);
 });
 
